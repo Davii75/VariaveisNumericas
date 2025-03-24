9f11b1aa1e65e108e3a5582d1825d784e83e4ab2
LAB53 <lab53@fatec.com>
2025-03-24 09:04:01 -0300

    Finaliza aula do dia 24/03

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Formação de string:
+// string nome;
+// string sobrenome;
+
+// Valores literais
+// nome = "Davi";
+// sobrenome = "Oliveira";
+
+// Valores fornecidos  pelo usuário
+// nome = Console.ReadLine();
+// sobrenome = Console.ReadLine();
+
+//  Outra forma de declarar variável numérica: int numero01, int numero02;  
+// Converter texto para número = Convert.To(x) 
+// Exemplo: Convert.ToDouble; Convert.ToInt32; Convert.ToDecimal; entre outros. 
+
+// double numero01;
+// double numero02;
+// double resultado;
+
+// Console.Write("Digite um número: ");
+// numero01 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Digite outro número: ");
+// numero02 = Convert.ToDouble(Console.ReadLine());
+
+// resultado = numero01 / numero02;
+// Console.WriteLine($"O cálculo de: {numero01} / {numero02} é aproximadamente igual a: = {resultado:N3}");
+
+// Fórmula do Pi =  pi * r²
+
+// const double ValorAproximadoPi = 3.1416;
+// double raio;
+// double areaCirculo; 
+// 
+// Console.WriteLine("------- Área do Círculo -------");
+// Console.WriteLine("    Informe o valor do raio: ");
+// raio = Convert.ToDouble(Console.ReadLine());
+// areaCirculo = ValorAproximadoPi * Math.Pow(raio, 2);
+// Console.WriteLine($"A área desse círculo é {areaCirculo}");
